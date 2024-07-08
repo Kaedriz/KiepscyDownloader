6b243637867ce5ee2497bc0e2503734a908f58d4
Kaedriz <kaedriz@proton.me>
2024-07-08 19:50:32 +0200

    Change to single quotes

--- downloader.ts
+++ downloader.ts
@@ -1,29 +1,26 @@
-import progressBar from "./progressIndicator";
+import progressBar from './progressIndicator';
 
 export default async function download(URL: string) {
 	const response = await fetch(URL);
 
-	console.log("Downloading...");
+	console.log('Downloading...');
 
 	// Throw error if problem with fetching
-	if (!response.ok) throw new Error("Problem with downloading content.");
+	if (!response.ok) throw new Error('Problem with downloading content.');
 
 	// Get content length
-	const contentLength = response.headers.get("Content-Length") as string;
-	const contentLengthMb = Math.floor(
-		Number.parseInt(contentLength) / 1024 / 1024
-	);
+	const contentLength = response.headers.get('Content-Length') as string;
+	const contentLengthMb = Math.floor(Number.parseInt(contentLength) / 1024 / 1024);
 
 	// Variable for received bits
 	let receivedLength = 0;
 
 	// Start reader for response
-	const reader =
-		response?.body?.getReader() as ReadableStreamDefaultReader<Uint8Array>;
+	const reader = response?.body?.getReader() as ReadableStreamDefaultReader<Uint8Array>;
 
 	// Start progress bar
 	progressBar.start(contentLengthMb, 0, {
-		speed: "N/A",
+		speed: 'N/A'
 	});
 
 	// TODO: Add download speed indicator
@@ -40,9 +37,7 @@ export default async function download(URL: string) {
 		receivedLength += value.length;
 
 		// Calculate current progress
-		const progress = Math.floor(
-			(receivedLength / Number.parseInt(contentLength)) * contentLengthMb
-		);
+		const progress = Math.floor((receivedLength / Number.parseInt(contentLength)) * contentLengthMb);
 
 		// Update progress indicator
 		progressBar.update(progress);

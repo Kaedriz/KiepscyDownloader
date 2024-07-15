95e8c3340086f6abf2a07fd9d9bbb23f759b0d2f
Kaedriz <kaedriz@proton.me>
2024-07-15 22:14:05 +0200

    🐞 fix(regexes.ts): fix greedy regex
    Regex is now constrained by word boundaries, to prevent overreaching

--- regexes.ts
+++ regexes.ts
@@ -3,7 +3,7 @@ export const illegalCharactersRegex = /[^SE\d\-]/i;
 export const multipleEpisodesRangeRegex =
 	/[S](?<First_Season_Number>[0-9]{1,2})[E](?<First_Episode_Number>[0-9]{1,2})[-](?:[S](?<Second_Season_Number>[0-9]{1,2}))?(?:[E](?<Second_Episode_Number>[0-9]{1,2}))$/i;
 
-export const episodeRegex = /[S](?<First_Season_Number>[0-9]{1,2})[E](?<First_Episode_Number>[0-9]{1,2})$/i;
+export const episodeRegex = /^(?:[S](?<First_Season_Number>[0-9]{1,2})[E](?<First_Episode_Number>[0-9]{1,2}))$/i;
 
 export const multipleSeasonsRangeRegex =
 	/[S](?<First_Season_Number>[0-9]{1,2})-?([S](?<Second_Season_Number>[0-9]{1,2}))?$/i;

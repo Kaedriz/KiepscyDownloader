543aa2cfa9e5126fb459f5b7e4f0226e28bef135
Kaedriz <kaedriz@proton.me>
2024-07-15 20:48:42 +0200

    Fix regex's support for required dashes

--- regexes.ts
+++ regexes.ts
@@ -1,7 +1,7 @@
 export const illegalCharactersRegex = /[^SE\d\-]/i;
 
 export const multipleEpisodesRangeRegex =
-	/[S](?<First_Season_Number>[0-9]{1,2})[E](?<First_Episode_Number>[0-9]{1,2})(?:[-][S](?<Second_Season_Number>[0-9]{1,2}))?(?:[-][E](?<Second_Episode_Number>[0-9]{1,2}))?$/i;
+	/[S](?<First_Season_Number>[0-9]{1,2})[E](?<First_Episode_Number>[0-9]{1,2})[-](?:[S](?<Second_Season_Number>[0-9]{1,2}))?(?:[E](?<Second_Episode_Number>[0-9]{1,2}))$/i;
 
 export const episodeRegex = /[S](?<First_Season_Number>[0-9]{1,2})[E](?<First_Episode_Number>[0-9]{1,2})$/i;
 
